9d01072c8ba94d32ca23f627e8d64eff0899c26d
zhfrn16 <63395988+zhfrn16@users.noreply.github.com>
2022-01-09 23:11:26 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,4 +7,10 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     //
+    
+    public function getBarber(){
+      $results = app('db')->select("SELECT * FROM barber"); 
+    return response()->json($results);
+    }
+
 }
